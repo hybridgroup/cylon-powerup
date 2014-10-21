9aeb3fafc7cde80f4af6f65e06d8f5b5b7ea0bb2
Adrian Zankich <adrian@hybridgroup.com>
2014-10-21 14:02:28 -0700

    rudder and thrust work

---
+++ examples/powerup_dualshock3.js
@@ -0,0 +1,49 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: [
+    { name: 'bluetooth', adaptor: 'ble', uuid: '84dd20eb3d89' },
+    { name: 'dualshock3', adaptor: 'joystick' }
+  ],
+  devices: [
+    { name: 'controller', driver: 'dualshock-3', connection: 'dualshock3' }, 
+    { name: 'powerup', driver: 'powerup' }
+  ],
+  work: function(my) {
+    var thrust = 0;
+    var rudder = 0;
+    var canRudder = false;
+
+    var cb = function(err) {
+      if (!!err) {
+        console.log(err);
+      } else {
+        if (canRudder) {
+          canRudder = false;
+          my.powerup.setRudder(rudder, cb);
+        }
+        else {
+          my.powerup.setThrust(thrust, cb);
+        }
+      }
+    }
+
+    my.powerup.setThrust(thrust, cb);
+
+    my.controller.on("left_y:move", function(data) {
+      if (data < 0) {
+        thrust = Math.abs(data/32768*254) | 0;
+      } else {
+        thrust = 0;
+      }
+    });
+
+    my.controller.on("right_x:move", function(data) {
+      var tmp = data/32768*127 | 0;
+      if (tmp !== rudder) {
+        rudder = tmp;
+        canRudder = true;
+      }
+    });
+  }
+}).start();
